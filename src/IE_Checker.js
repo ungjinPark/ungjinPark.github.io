c0bd69d04536d10b4cb3eaff0c0ea0741f7f8511
ungjinpark <ungjin312@kakao.com>
2019-12-31 12:47:32 +0900

    bug fix from blocking IE js file

--- src/IE_Checker.js
+++ src/IE_Checker.js
@@ -1,5 +1,5 @@
 function isIE(){
-	if(navigator.userAgent.indexOf('Trident') > -1 && navigator.userAgent.indexOf('MSIE') > -1 ){
+	if(navigator.userAgent.indexOf('Trident') > -1 || navigator.userAgent.indexOf('MSIE') > -1 ){
 		return true;
 	}else{
 		return false;
